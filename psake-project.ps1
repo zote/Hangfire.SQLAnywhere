1096aba9d847ef47a277aa42360924fdbc7d1103
PagueVeloz <github@pagueveloz.com.br>
2015-04-22 14:39:58 -0300

    Ajustes inciais
    Renomeados mais arquivos.

--- psake-project.ps1
+++ psake-project.ps1
@@ -1,5 +1,5 @@
 Properties {
-    $solution = "Hangfire.Firebird.sln"
+    $solution = "Hangfire.SQLAnywhere.sln"
 }
 
 Include "packages\Hangfire.Build.*\tools\psake-common.ps1"
@@ -7,29 +7,29 @@ Include "packages\Hangfire.Build.*\tools\psake-common.ps1"
 Task Default -Depends Collect
 
 Task Test -Depends Compile -Description "Run unit and integration tests." {
-    Run-XunitTests "Hangfire.Firebird.Tests"
-	Run-XunitTests "Hangfire.Firebird.Msmq.Tests"
+    Run-XunitTests "Hangfire.SQLAnywhere.Tests"
+	Run-XunitTests "Hangfire.SQLAnywhere.Msmq.Tests"
 }
 
 Task Merge -Depends Test -Description "Run ILMerge /internalize to merge assemblies." {
     # Remove `*.pdb` file to be able to prepare NuGet symbol packages.
-    Remove-Item ((Get-SrcOutputDir "Hangfire.Firebird") + "\Dapper.pdb")
+    Remove-Item ((Get-SrcOutputDir "Hangfire.SQLAnywhere") + "\Dapper.pdb")
 
-    Merge-Assembly "Hangfire.Firebird" @("Dapper")
+    Merge-Assembly "Hangfire.SQLAnywhere" @("Dapper")
 }
 
 Task Collect -Depends Merge -Description "Copy all artifacts to the build folder." {
-    Collect-Assembly "Hangfire.Firebird" "Net45"
-	Collect-Assembly "Hangfire.Firebird.Msmq" "Net45"
+    Collect-Assembly "Hangfire.SQLAnywhere" "Net45"
+	Collect-Assembly "Hangfire.SQLAnywhere.Msmq" "Net45"
 
-    Collect-Tool "src\Hangfire.Firebird\Install.v1.sql"
+    Collect-Tool "src\Hangfire.SQLAnywhere\Install.v1.sql"
 }
 
 Task Pack -Depends Collect -Description "Create NuGet packages and archive files." {
     $version = Get-BuildVersion
 
-    Create-Archive "Hangfire-Firebird-$version"
+    Create-Archive "Hangfire-SQLAnywhere-$version"
 
-    Create-Package "Hangfire.Firebird" $version
-	Create-Package "Hangfire.Firebird.Msmq" $version
+    Create-Package "Hangfire.SQLAnywhere" $version
+	Create-Package "Hangfire.SQLAnywhere.Msmq" $version
 }
